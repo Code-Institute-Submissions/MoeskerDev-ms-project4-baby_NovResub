fc4b2157b5fc7241da52091432eee6613be21834
MoeskerDev <mgmoesker@gmail.com>
2021-10-02 22:53:59 +0000

    add products app, models and images

--- baby/settings.py
+++ baby/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m0scgzmf09o_fvq6p08chze6pfuat@!5&ynmfqb*)c6md#vca2'
+SECRET_KEY = (
+    'django-insecure-m0scgzmf09o_fvq6p08chze6pfuat@!5&ynmfqb*)c6md#vca2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -115,16 +117,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+      'NAME': (
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+      'NAME': (
+        'django.contrib.auth.password_validation.MinimumLengthValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+      'NAME': (
+        'django.contrib.auth.password_validation.CommonPasswordValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+      'NAME': (
+        'django.contrib.auth.password_validation.NumericPasswordValidator'),
     },
 ]
 
