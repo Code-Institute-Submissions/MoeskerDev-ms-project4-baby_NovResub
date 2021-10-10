74963ea4d82c0cc59e8498c39f42ceaea13a64f1
MoeskerDev <mgmoesker@gmail.com>
2021-10-10 23:39:41 +0000

    update webhook handler to handle profiles

--- profiles/forms.py
+++ profiles/forms.py
@@ -30,5 +30,6 @@ class UserProfileForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-secondary rounded-pill profile-form-input'
+            self.fields[field].widget.attrs['class'] = (
+                'border-secondary rounded-pill profile-form-input')
             self.fields[field].label = False
