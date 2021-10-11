d7f4773396271b70f4849dae39276857bb03f36b
MoeskerDev <mgmoesker@gmail.com>
2021-10-11 16:46:16 +0000

    add procfile and allowed hosts

--- baby/settings.py
+++ baby/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['babyadmin4-baby.herokuapp.com', 'localhost']
 
 # Application definition
 
