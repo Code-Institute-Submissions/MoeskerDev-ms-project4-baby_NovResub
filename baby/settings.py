7b27cb46b95b2e3634b53eb789ca7c2e611ed74e
MoeskerDev <mgmoesker@gmail.com>
2021-10-11 18:18:05 +0000

    adjust secret key and set debug

--- baby/settings.py
+++ baby/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # ('django-insecure-m0scgzmf09o_fvq6p08chze6pfuat@!5&ynmfqb*)c6md#vca2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['babyadmin4-baby.herokuapp.com', 'localhost']
 
