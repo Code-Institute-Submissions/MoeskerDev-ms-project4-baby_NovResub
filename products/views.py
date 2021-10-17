cb2a98f43d981132927b6b7f54b39f357f956604
MoeskerDev <mgmoesker@gmail.com>
2021-10-17 15:28:13 +0000

    solve server error 500 product detail page

--- products/views.py
+++ products/views.py
@@ -63,8 +63,8 @@ def all_products(request):
 
 def product_detail(request):
     """ A view to show individual product details """
-
-    product = get_object_or_404(Product, pk=id)
+    
+    product = get_object_or_404(Product, pk=product_id)
     # review = get_object_or_404(Review, pk=id)  # or user id?
 
     context = {
