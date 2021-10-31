d401e73d9200c4cfcf42180b268cb1e63c7bdbcf
MoeskerDev <mgmoesker@gmail.com>
2021-10-31 08:20:02 +0000

    add pylintrc file

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,19 @@
+"""
+To create a separate storage of static and media files
+"""
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    """
+    To set a location for static files
+    """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """
+    To set a location for media files
+    """
     location = settings.MEDIAFILES_LOCATION
